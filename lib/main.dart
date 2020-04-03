17379842b659b20819d4e9fb12490df1480d7075
EvertonFi <jose.everton.f@hotmail.com>
2020-04-03 16:00:29 -0300

    Update Totais

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   var dados;
+  int totalcasos = 0;
+  int totalmortes = 0;
   bool carregado = false;
 
   getInfoVirus() async {
@@ -37,10 +39,11 @@ class _HomePageState extends State<HomePage> {
       setState(() {
         dados = map;
         for (var i = 0; i < Et.estados.length; i++) {
-          print(dados[Et.estados[i]]);
-          print(i);
+          totalcasos = (totalcasos + dados[Et.estados[i]]["confirmed"].toInt());
+          totalmortes = (totalmortes + dados[Et.estados[i]]["deaths"].toInt());
           carregado = true;
         }
+        print(totalcasos);
       });
     });
   }
@@ -53,38 +56,69 @@ class _HomePageState extends State<HomePage> {
         child: Container(
           height: MediaQuery.of(context).size.height,
           child: carregado
-              ? ListView.builder(
-                  itemCount: Et.estados.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Center(
-                        child: Container(
-                          height: 100,
-                          child: Column(
-                            children: <Widget>[
-                              Text(
-                                "Estado: " +
-                                    dados[Et.estados[index]]
-                                        ["name"],
-                                style: TextStyle(fontSize: 25),
-                              ),
-                              Text(
-                                  "Confirmados: " +
-                                      dados[Et.estados[index]]["confirmed"]
-                                          .toString(),
-                                  style: TextStyle(fontSize: 20)),
-                              Text(
-                                  "Mortos: " +
-                                      dados[Et.estados[index]]["deaths"]
-                                          .toString(),
-                                  style: TextStyle(fontSize: 20))
-                            ],
+              ? Column(
+                  children: <Widget>[
+                    Container(
+                      color: Colors.white,
+                      width: MediaQuery.of(context).size.width,
+                      height: 200,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(25),
+                            child: Text(
+                              "Total de Casos: " + totalcasos.toString(),
+                              style: TextStyle(fontSize: 25),
+                            ),
                           ),
-                        ),
+                          Padding(
+                            padding: const EdgeInsets.all(25),
+                            child: Text(
+                              "Total de Mortes: " + totalmortes.toString(),
+                              style: TextStyle(fontSize: 25),
+                            ),
+                          )
+                        ],
                       ),
-                    );
-                  },
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height - 200,
+                      child: ListView.builder(
+                        itemCount: Et.estados.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Center(
+                              child: Container(
+                                height: 100,
+                                child: Column(
+                                  children: <Widget>[
+                                    Text(
+                                      "Estado: " +
+                                          dados[Et.estados[index]]["name"],
+                                      style: TextStyle(fontSize: 25),
+                                    ),
+                                    Text(
+                                        "Confirmados: " +
+                                            dados[Et.estados[index]]
+                                                    ["confirmed"]
+                                                .toString(),
+                                        style: TextStyle(fontSize: 20)),
+                                    Text(
+                                        "Mortos: " +
+                                            dados[Et.estados[index]]["deaths"]
+                                                .toString(),
+                                        style: TextStyle(fontSize: 20))
+                                  ],
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
                 )
               : Container(
                   child: Center(
